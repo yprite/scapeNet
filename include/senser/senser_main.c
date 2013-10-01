4ff8cf3f09b4fbe5ad7fb0f1042cce91e398f0c5
kwonhoegeun <hoeguen.kwon@jjssm.org>
2013-10-01 11:15:12 +0900

    nodekill complete

--- include/senser/senser_main.c
+++ include/senser/senser_main.c
@@ -1,14 +1,38 @@
-
 #include <stdio.h>
 #include "senser_networkScan.h"
 
 int main(int argc, char *argv[])
 {
-	// int i;
-	NodeStatus node_status;
-	memset(&node_status, 0, sizeof(NodeStatus));
+	network_grub_args n_args;
+	pthread_t t_id1;
+	int state1 = 0;
+	int i;
+	
 	printf("network Scaning\n");
+	memset(&n_args, 0, sizeof(network_grub_args));
+	n_args.g_ip = 1;
+	
+	state1 = pthread_create(&t_id1, NULL, networkScan, &n_args);
+	if(state1 != 0) {
+		fprintf(stderr, "pthread_create() error\n");
+		return 0;
+	}
 
-	networkScan(&node_status);
+	printf("count = %d\n", argc);
+
+	if(argc > 1) {
+		for(i=1; i<argc; i++) {
+			int t_ip = atoi(argv[i]);
+			printf("%d\n",t_ip);
+			n_args.k_list.target_ip[t_ip] = 1;
+		}
+	}
+
+	while(1) {
+		sleep(60);
+	}
+
+
+
 	return 0;
 }
--- include/senser/senser_networkScan.h
+++ include/senser/senser_networkScan.h
@@ -37,18 +37,40 @@ typedef struct NodeStatus {
 	NodeInfo node[255];
 } NodeStatus;
 
-typedef struct grub_args {
+typedef struct killnode_list {
+	u_char target_ip[255];
+} killnode_list;
+
+typedef struct network_grub_args {
+	killnode_list k_list;
+	u_char g_ip;
+} network_grub_args;
+
+typedef struct receiver_grub_args {
 	pcap_t *p_descr;
 	NodeStatus *p_node_status;
 	unsigned char source_ip[4];
-} grub_args;
+} receiver_grub_args;
+
+typedef struct sendkill_grub_args {
+	pcap_t *descr;
+	device_info *gate_info;
+	network_grub_args *n_args;
+} sendkill_grub_args;
 
 void *networkScan(void *);
 void send_arp_packet(pcap_t *, device_info);
 unsigned char* make_arp_packet(device_info, u_char );
+
 int get_device_info(device_info *);
 void print_packet(const unsigned char *);
+
 void *receiver(void *);
 int check_reply_packet(const unsigned char *, struct pcap_pkthdr *, unsigned char *, NodeStatus *, int);
 void confirmNodeTraffic(const unsigned char *, struct pcap_pkthdr *, unsigned char *, int);
+
+void *send_kill_packet(void *);
+unsigned char* make_kill_packet(device_info , u_char , u_char );
+int gateway_get(const unsigned char *, u_char , device_info *);
+
 #endif
