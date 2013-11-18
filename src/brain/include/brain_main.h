9d5fcd421b19a43d14d94b8a914289766338be22
yprite <yprite@naver.com>
2013-11-18 17:10:15 +0900

    brain 기본 헤더

---
+++ src/brain/include/brain_main.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
+#define EPOLL_SIZE 20
+#define BUFFER_SIZE 255
+
+
+typedef struct u_info{
+
+    char source_ip[12];
+    char port[5][10];
+    char up_traffic_cur[10];
+    char down_traffic_cur[10];
+    char up_traffic_limit[10];
+    char down_traffic_limit[10];
+    int isStatus;
+
+}u_info;
