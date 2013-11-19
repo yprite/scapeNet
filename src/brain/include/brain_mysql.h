f3333d64170d5359a2e3023a5713f2aa72c744b3
yprite <yprite@naver.com>
2013-11-19 21:51:30 +0900

    brain_mysql.h 수정 2차

--- src/brain/include/brain_mysql.h
+++ src/brain/include/brain_mysql.h
@@ -8,6 +8,9 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 
+//pthread header
+#include <pthread.h>
+
 //mysq header
 #include "/usr/include/mysql/mysql.h"
 
@@ -37,13 +40,14 @@ typedef struct u_info{
     char source_ip[20];
     char mac[20];
     char port[5][10];
-    char up_traffic_cur[10];
-    char down_traffic_cur[10];
+    int up_traffic_cur;
+    int down_traffic_cur;
     char up_traffic_limit[10];
     char down_traffic_limit[10];
     int isStatus;
 }u_info;
 u_info user_info[255];
+int isTime;
 
 //Mysql function
 int brain_mysql_init();
@@ -55,4 +59,4 @@ int brain_mysql_data_copy();
 
 //Main function
 int brain_main_init_data(int);// parameter 0: mysql, 1: sshdb 
-
+void *timer_function(void *data);
