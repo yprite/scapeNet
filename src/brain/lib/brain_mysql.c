114b8f819caa41bfa8b4c0989aeb534aaa478f65
yprite <yprite@yprite.(none)>
2013-11-27 03:45:51 +0900

    최종수정

--- src/brain/lib/brain_mysql.c
+++ src/brain/lib/brain_mysql.c
@@ -84,13 +84,13 @@ int brain_init_db(int mode){
 			return -1;
 		}
 		for(ip=1; ip<255; ip++){
-			sprintf(temp, "update ip_data set isStatus = 0 where pid = %d", ip);
+			sprintf(temp, "update ip_data set isStatus = 0 ,up_traffic_cur =0, down_traffic_cur =0 where pid = %d", ip);
 //			printf("temp : %s\n", temp);
 			if(brain_mysql_load(temp) != 0){
 				printf("return value : -2 |error in brain_init_db()\n");
 				return -2;
 			}
-			usleep(10);
+//			usleep(10);
 		}
 		mysql_close(connection);
 	}
@@ -123,55 +123,95 @@ int brain_load_db(int mode){
 	}
 	return 0;
 }
-int brain_update_db(int mode, int ip, int isOn){
+int brain_update_db(int mode, int ip, int isOn, int up_traffic_cur,int down_traffic_cur){
 
 	if(mode == 0){
-		char temp[BUFFER_SIZE];
-		sprintf(temp, "update ip_data set isStatus = %d where pid = %d", isOn, ip);
-		//printf("temp : %s\n", temp);
 		if(brain_mysql_init() != 0){
 			printf(" return value : -1 |error in brain_update_db()\n");
 			return -1;
 		}
+		char temp[BUFFER_SIZE];
+		sprintf(temp, "update ip_data set isStatus = %d, up_traffic_cur = %d, down_traffic_cur = %d where pid = %d", isOn, up_traffic_cur, down_traffic_cur, ip);
 		if(brain_mysql_load(temp) != 0){
 			printf(" return value : -2 |error in brain_update_db()\n");
 			return -2;
 		}
-/*		if(brain_mysql_data_return() != 0){
-			printf(" return value : -3 |error in brain_update_db\n");
-			return -3;
-		}*/
-//		brain_mysql_data_free();
 		mysql_close(connection);
-
 	}
 	else if(mode ==1){
-		printf("SSHDB update\n");
+		if(brain_mysql_init() != 0){
+			printf(" return value : -1 |error in brain_update_db()\n");
+			return -1;
+		}
+		char temp[BUFFER_SIZE];
+		sprintf(temp, "update ip_data set isStatus = 0");
+		if(brain_mysql_load(temp) != 0){
+			printf(" return value : -2 |error in brain_update_db()\n");
+			return -2;
+		}
+		mysql_close(connection);
 	}
 	else{
 		printf("mode uselected\n");
 		return 1;
 	}
+//	usleep(10);
 	return 0;
 } 
+int write_logfile(char *buffer){
+
+	time_t timer;
+	struct tm *t;
+	
+	char date[9];
+	int fileAccess;
+	FILE *fp;
+	
+	timer = time(NULL);
+	t = localtime(&timer);
+	sprintf(date, "../../log/%d%d%d.log", t->tm_year+1900, t->tm_mon, t->tm_mday);
+	fileAccess = access(date, R_OK|W_OK);
+
+	if(fileAccess ==0){
+		fp  = fopen(date, "a+");
+	}
+	else{
+		fp = fopen(date, "w+");
+	}
+	
+	if(fp == NULL){
+		perror("fail to fopen");
+		return -10;		
+	}
+	
+	fseek(fp, 0, SEEK_END);
+	
+//	fwrite(buffer, 1, strlen(buffer), fp);
+	fclose(fp);
+
+	return 0;
+}
+
 
 void *timer_function(void *data){
 	while(1){
 		if(isTime == -1 || isTime == 2){
 //			printf("\t\t\t\t\t\ttime strat\n");
-			usleep(20000);
+			sleep(2);
+//			usleep(10000);
 //			printf("\t\t\t\t\t\ttime end\n");
 			isTime = 2 ;
+			isTimeOn ++;
 		}	
     	}
 }
 void *check_Node(void *data){
-	int ip=0;
-	printf("Check Thread Start\n");
+//	int ip=0;
+	sleep(11);
+	isChecked=2;
 	while(1){
-		sleep(3);
-		if(isCheckNode == 1){
-			for(ip=1; ip<255; ip++){
+//		if(isCheckNode == 1){
+/*			for(ip=1; ip<255; ip++){
 				printf("%d -> %d : %d\n", ip,user_info[ip].isStatus, isNodeLive[ip]);
 				if(user_info[ip].isStatus != isNodeLive[ip]){
 					printf("%d is off \n", ip);
@@ -181,7 +221,11 @@ void *check_Node(void *data){
 			}
 			for(ip=0; ip<255; ip++){
 				isNodeLive[ip] = 0;
-			} 		
+			} 		*/
+		if(isChecked == 2){
+			sleep(3);
+			isChecked=1;
 		}
+		
 	}
 }
