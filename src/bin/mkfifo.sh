9a69a5b61d3dfc21266261ba8064c8ccf93e3242
vm_thor <khg@ubuntu.(none)>
2013-12-01 09:03:57 -0800

    mkfifo read_sense3 추가

--- src/bin/mkfifo.sh
+++ src/bin/mkfifo.sh
@@ -2,6 +2,7 @@
 
 mkfifo --mode 0666 /tmp/read_sense
 mkfifo --mode 0666 /tmp/read_sense2
+mkfifo --mode 0666 /tmp/read_sense3
 mkfifo --mode 0666 /tmp/write_sense
 mkfifo --mode 0666 /tmp/read_face
 mkfifo --mode 0666 /tmp/write_face
