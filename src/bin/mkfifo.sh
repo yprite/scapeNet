f1ecddb482fbef887c272e966be8cd537f6db209
vm_thor <khg@ubuntu.(none)>
2013-12-01 09:00:02 -0800

    mkfifo 경로수정

--- src/bin/mkfifo.sh
+++ src/bin/mkfifo.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkfifo --mode 0666 read_sense
-mkfifo --mode 0666 read_sense2
-mkfifo --mode 0666 write_sense
-mkfifo --mode 0666 read_face
-mkfifo --mode 0666 write_face
-mkfifo --mode 0666 write_php
+mkfifo --mode 0666 /tmp/read_sense
+mkfifo --mode 0666 /tmp/read_sense2
+mkfifo --mode 0666 /tmp/write_sense
+mkfifo --mode 0666 /tmp/read_face
+mkfifo --mode 0666 /tmp/write_face
+mkfifo --mode 0666 /tmp/write_php
 
