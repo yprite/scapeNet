58d8512a8ca28711a64cb75f8e4fa81c933d0fa0
leegwiro <leegwiro@naver.com>
2013-12-08 19:40:35 +0900

    Change script

--- scripts/qos_setting.sh
+++ scripts/qos_setting.sh
@@ -7,4 +7,4 @@ sudo tc class add dev eth0 parent 1: classid 1:3 htb rate 5kbps ceil 5kbps
 sudo tc class add dev eth0 parent 1: classid 1:4 htb rate 7kbps ceil 7kbps
 sudo tc class add dev eth0 parent 1: classid 1:5 htb rate 10kbps ceil 10kbps
 
-sudo tc filter add dev eth0 protocol ip parent 1:0 prio 1 u32 match ip src 210.118.34.28/32 flowid 1:3
+#sudo tc filter add dev eth0 protocol ip parent 1:0 prio 1 u32 match ip src 210.118.34.28/32 flowid 1:3
