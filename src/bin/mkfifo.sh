ea75b9290b168e6208d1c03a3fa3c81c0b096853
root <root@ubuntu.(none)>
2013-12-08 21:05:33 -0800

    Change mkfifo.sh

--- src/bin/mkfifo.sh
+++ src/bin/mkfifo.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-READ_FACE=/tmp/read_face
 READ_SENSE=/tmp/read_sense
 READ_SENSE2=/tmp/read_sense2
 READ_SENSE3=/tmp/read_sense3
@@ -12,7 +11,6 @@ WRITE_SENSE=/tmp/write_sense
 READ_FACE=/home/scapeNet/www/bin/read_face
 READ_FACE2=/home/scapeNet/www/bin/read_face2
 
-if [ ! -e $READ_FACE ]; then mkfifo --mode 0666 $READ_FACE; fi
 if [ ! -e $READ_SENSE ]; then mkfifo --mode 0666 $READ_SENSE; fi
 if [ ! -e $READ_SENSE2 ]; then mkfifo --mode 0666 $READ_SENSE2; fi
 if [ ! -e $READ_SENSE3 ]; then mkfifo --mode 0666 $READ_SENSE3; fi
@@ -21,7 +19,7 @@ if [ ! -e $WRITE_TRAFFIC ]; then mkfifo --mode 0666 $WRITE_TRAFFIC; fi
 if [ ! -e $WRITE_KILL ]; then mkfifo --mode 0666 $WRITE_KILL; fi
 if [ ! -e $WRITE_PORT ]; then mkfifo --mode 0666 $WRITE_PORT; fi
 if [ ! -e $WRITE_SENSE ]; then mkfifo --mode 0666 $WRITE_SENSE; fi
-if [ ! -e $READ_FACE]; then mkfifo --mode 0666 $READ_FACE; fi
-if [ ! -e $READ_FACE2]; then mkfifo --mode 0666 $READ_FACE2; fi
+if [ ! -e $READ_FACE ]; then mkfifo --mode 0666 $READ_FACE; fi
+if [ ! -e $READ_FACE2 ]; then mkfifo --mode 0666 $READ_FACE2; fi
 
 
